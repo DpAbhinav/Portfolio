1c91f4a6fcc143ef7952418831b42bf89aed1589
Abhishek Kumar Mallik <abhi.2178.akm@gmail.com>
2025-07-19 02:04:28 +0530

    framegrab removed

--- app/page.js
+++ app/page.js
@@ -14,7 +14,7 @@ export default function Home() {
       <Navbar />
       <Header />
       <Gallery/>
-        <MasonryGallery />
+        {/*<MasonryGallery />*/}
       <Work />  
       <About />
       <Contact/>
