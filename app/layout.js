eb7bd702b3923fd6e37bdda2d9247e213eb97be0
Abhishek Kumar Mallik <abhi.2178.akm@gmail.com>
2025-07-19 22:25:55 +0530

    script test_2

--- app/layout.js
+++ app/layout.js
@@ -1,6 +1,6 @@
 import { Outfit, Ovo } from "next/font/google";
 import "./globals.css";
-import Script from "next/script"; // ← Import Script
+import Script from "next/script";
 
 const outfit = Outfit({
     subsets: ["latin"],
@@ -20,17 +20,15 @@ export const metadata = {
 export default function RootLayout({ children }) {
     return (
         <html lang="en" className="scroll-smooth">
-        <head>
-            {/* ✅ Cloudflare Analytics Script */}
-            <Script
-                src="https://static.cloudflareinsights.com/beacon.min.js"
-                data-cf-beacon='{ "token": "07b96f409d8949f4a11483e27783737b"}'
-                strategy="afterInteractive"
-            />
-        </head>
         <body
             className={`${outfit.className} ${ovo.className} antialiased leading-8 overflow-x-hidden scroll-smooth`}
         >
+        {/* ✅ Cloudflare Analytics Script placed just inside <body> */}
+        <Script
+            src="https://static.cloudflareinsights.com/beacon.min.js"
+            data-cf-beacon='{"token": "07b96f409d8949f4a11483e27783737b"}'
+            strategy="afterInteractive"
+        />
         {children}
         </body>
         </html>
